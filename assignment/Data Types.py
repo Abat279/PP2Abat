2108ae35d82b279fb72cbc55b8e03dd8e768f989
Abat279 <>
2026-01-29 18:32:07 +0300

    Add Practice1

--- assignment/Data Types.py
+++ assignment/Data Types.py
@@ -1,8 +1,17 @@
+
 x = 5
 print(type(x))
 x = ["apple", "banana", "cherry"]
 print(type(x))
-x = {"name" : "John", "age" : 36}
+
+
+x = {"name": "Taka", "age": 36}
 print(type(x))
-x = frozenset({"apple", "banana", "cherry"})
+
+
+x = frozenset({"pen", "pencil", "eraser"})
+print(type(x))
+
+
+x = ("car", "dog", "cat")
 print(type(x))
