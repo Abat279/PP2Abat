28b5ba5af2f0945d94c3a99f4564bef4ad0928cd
Abat279 <>
2026-01-29 18:42:31 +0300

    Add Practice1

--- assignment/Comments.py
+++ assignment/Comments.py
@@ -1,6 +1,9 @@
 #This is a comment
+""""Astana,Atyrau"""
 #This is a comment
+"""Salem alem"""
 #This is a comment
+"""Hello world """
 #This is a comment
 """This is a comment
 that spans multiple lines"""
