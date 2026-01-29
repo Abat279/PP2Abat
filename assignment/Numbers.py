def2deef7ca2cf0c4915decf2d4db1fe3fb7910a
Abat279 <>
2026-01-29 18:08:14 +0300

    Add Practice1

--- assignment/Numbers.py
+++ assignment/Numbers.py
@@ -1,21 +1,38 @@
-x = 1    # int
-y = 2.8  # float
-z = 1j   # complex
-print(type(x))
-print(type(y))
-print(type(z))
-x = 1
-y = 35656222554887711
-z = -3255522
-
+x = 7        
+y = 4.5      
+z = 6j       
 print(type(x))
 print(type(y))
 print(type(z))
 
-x = 35e3
-y = 12E4
-z = -87.7e100
 
+a = 0
+b = -25
+c = 123456789012345
+print(type(a))
+print(type(b))
+print(type(c))
+
+
+x = 9.8
+y = -0.001
+z = 5e2
+print(type(x))
+print(type(y))
+print(type(z))
+
+
+x = 1j
+y = -3j
+z = 10 + 5j
+print(type(x))
+print(type(y))
+print(type(z))
+
+
+x = 2E3
+y = 6.7e-4
+z = -9.1E8
 print(type(x))
 print(type(y))
 print(type(z))
