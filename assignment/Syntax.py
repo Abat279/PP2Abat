caee7e88ec0c8d8a956af62e5cf0d23ae0e12b1e
Abat279 <>
2026-01-29 17:47:08 +0300

    B add practice

--- assignment/Syntax.py
+++ assignment/Syntax.py
@@ -1,12 +1,12 @@
 if 5 > 2:
   print("Five is greater than two!")
-if 5 > 2:
-  print("Five is greater than two!")
-if 5 > 2:
-  print("Five is greater than two!")
-if 5 > 2:
-  print("Five is greater than two!")
-if 5 > 2:
-  print("Five is greater than two!")
-if 5 > 2:
-  print("Five is greater than two!")
+if 6 > 2:
+  print("Six is greater than two!")
+if 5 > 1:
+  print("Five is greater than one!")
+if 10 > 2:
+  print("Ten is greater than two!")
+if 5 > 4:
+  print("Five is greater than four!")
+if 7 > 1:
+  print("Seven is greater than one!")
